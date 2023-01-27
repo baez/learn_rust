ec549d712fcf1bd39bb8cbc820653501afcda84a
GB Karim <bekarim@microsoft.com>
2023-01-26 18:06:25 -0800

    added pow examples

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,10 @@ fn main() {
     let mod_result:i32 = 32 % 5;
 
     println!("32 % 5 is {}", mod_result);
+
+    let result:i32 = i32::pow(8, 3);
+    println!("8 to the power of 3 is: {}", result);   
+
+    let result_fp:f32 = f32::powf(1.34, 0.3);
+    println!("1.34 to the power of 0.3 is: {}", result_fp);   
 }
